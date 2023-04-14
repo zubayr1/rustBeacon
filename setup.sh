f41cb26723b70d7d4ce6b0f83d86d9603252e92e
zubayr1 <write2zubayr@gmail.com>
2023-04-14 13:09:25 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+if [ -d "${folder}" ]; then
+  cd "${folder}"
+  git pull https://github.com/zubayr1/rustBeacon.git
+else
+  git clone https://github.com/zubayr1/rustBeacon.git
+  cd "${folder}"
+fi
+
+
+# run script
+cd "rusting"
+cargo run -- nok 1 4 03282129 dev 18.117.92.19 3
