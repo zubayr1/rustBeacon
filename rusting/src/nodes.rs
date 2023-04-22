bf423b2a1fc4b984352b5acf18896277212ed368
zubayr1 <write2zubayr@gmail.com>
2023-04-22 21:30:54 +0200

    decrease client waiting time

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -463,7 +463,7 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                             let handle1 = thread::spawn(move || {
                     
             
-                                let three_millis = time::Duration::from_millis(5);
+                                let three_millis = time::Duration::from_millis(3);
                                 thread::sleep(three_millis);
         
                                 let future = handle_client(ip.clone(), self_ip_clone.clone(), "none".to_string(), INITIAL_PORT+port_count, _index, behavior_clone.clone());
@@ -477,7 +477,7 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                         }
                         else 
                         {
-                            let three_millis = time::Duration::from_millis(10);
+                            let three_millis = time::Duration::from_millis(3);
                             thread::sleep(three_millis);
                             handle_client(ip.clone(), self_ip_clone.clone(), "none".to_string(), INITIAL_PORT+port_count, _index, behavior_clone.clone()).await;
                         }
