294c35dfc04120e277519920261e41a980349319
zubayr1 <write2zubayr@gmail.com>
2023-05-05 00:25:24 +0200

    fix nodes

--- rusting/src/nodes.rs
+++ rusting/src/nodes.rs
@@ -78,7 +78,7 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                         
                         thread::scope(|s| { // tokio thread, since leader is both client and server
                             s.spawn(|| {
-                                let blacklisted_child = server::handle_server("otherserver".to_string(), ip_address_clone.clone(), args_clone1.clone(), self_ip_clone1.clone(), INITIAL_PORT+port_count, _index, blacklisted.clone());
+                                let blacklisted_child = server::handle_server("selfserver".to_string(), ip_address_clone.clone(), args_clone1.clone(), self_ip_clone1.clone(), INITIAL_PORT+port_count, _index, blacklisted.clone());
                                 
                                 blacklisted.extend(blacklisted_child);
                             });
@@ -97,9 +97,27 @@ pub async fn initiate(ip_address: Vec<String>, args: Vec<String>)
                     }
                     else 
                     {   
-                        let three_millis = time::Duration::from_millis(1000);
-                        thread::sleep(three_millis);
-                        handle_client(ip.clone(), self_ip_clone.clone(), "none".to_string(), INITIAL_PORT+port_count, _index, behavior_clone.clone()).await;
+                        let ip_address_clone = ip_address.clone();
+                        let args_clone1 = args_clone.clone();
+                        let self_ip_clone1 = self_ip.clone();  
+
+                        
+                        thread::scope(|s| { // tokio thread, since leader is both client and server
+                            s.spawn(|| {
+                                let blacklisted_child = server::handle_server("otherserver".to_string(), ip_address_clone.clone(), args_clone1.clone(), self_ip_clone1.clone(), INITIAL_PORT+port_count, _index, blacklisted.clone());
+                                
+                                blacklisted.extend(blacklisted_child);
+                            });
+            
+                            s.spawn(|| {
+                                let three_millis = time::Duration::from_millis(1000);
+                                thread::sleep(three_millis);
+        
+                                let future = handle_client(ip.clone(), self_ip_clone.clone(), "none".to_string(), INITIAL_PORT+port_count, _index, behavior_clone.clone());
+        
+                                block_on(future);
+                            });
+                        });
                     }
 
                     
