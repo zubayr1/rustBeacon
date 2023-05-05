404c6170abca547c5bcea3854d536493debe1437
zubayr1 <write2zubayr@gmail.com>
2023-05-05 17:02:27 +0200

    change client

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -4,6 +4,7 @@ use tokio::fs::{OpenOptions};
 use tokio::net::TcpStream;
 use tokio::io::{ AsyncWriteExt};
 
+use std::{thread, time};
 
 #[path = "../crypto/schnorrkel.rs"]
 mod schnorrkel; 
@@ -38,7 +39,15 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
     let sign = fs::read_to_string("../sign.txt").expect("Unable to read file");
 
 
-    let stream = TcpStream::connect(address).await.unwrap(); //tokio TCPStream to connect to listening server
+    // let stream = TcpStream::connect(address).await.unwrap(); //tokio TCPStream to connect to listening server
+
+
+    let addressclone = address.clone();
+    while TcpStream::connect(addressclone.clone()).await.is_err() {
+        let three_millis = time::Duration::from_millis(10);
+        thread::sleep(three_millis);
+    }
+    let stream = TcpStream::connect(address).await.unwrap(); 
 
     let (_, mut write) = tokio::io::split(stream); 
 
