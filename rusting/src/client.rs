7cfe184891fab83f009d06dcdc0193e13e96df96
zubayr1 <write2zubayr@gmail.com>
2023-05-05 18:18:55 +0200

    client

--- rusting/src/client.rs
+++ rusting/src/client.rs
@@ -11,8 +11,7 @@ mod schnorrkel;
 
 #[tokio::main]
 pub async fn match_tcp_client(address: String, self_ip: String, types: String, epoch: i32, behavior: String)
-{
-    if !address.contains(&self_ip){
+{   
     let mut file = OpenOptions::new().append(true).open("output.log").await.unwrap();
 
     let mut text = ["epoch".to_string(), epoch.to_string()].join(": ");
@@ -49,6 +48,7 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
     while TcpStream::connect(addressclone.clone()).await.is_err() {
         let three_millis = time::Duration::from_millis(3);
         thread::sleep(three_millis);
+        println!("s");
     }
     if TcpStream::connect(addressclone.clone()).await.is_ok(){
 
@@ -91,5 +91,5 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
 
 }
         
-}
+
 }
