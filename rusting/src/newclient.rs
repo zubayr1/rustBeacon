0e38179d23fe3f29bd2600cead57b9353db081d0
zubayr1 <write2zubayr@gmail.com>
2023-05-05 18:44:45 +0200

    client

--- rusting/src/newclient.rs
+++ rusting/src/newclient.rs
@@ -21,6 +21,7 @@ pub async fn match_tcp_client(address: String, self_ip: String, types: String, e
 
         let three_millis = time::Duration::from_millis(3);
         thread::sleep(three_millis);
+        
 
     let mut stream = TcpStream::connect(address).await.unwrap();
 
